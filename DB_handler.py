8499cbac6d1204b49e1d5a65fec5770121c97f0e
Syno <t3n4ci0us@naver.com>
2022-04-05 23:25:47 +0900

    2

--- DB_handler.py
+++ DB_handler.py
@@ -1,3 +1,4 @@
+from asyncio.format_helpers import _format_callback_source
 import pyrebase
 import json
 
@@ -6,10 +7,36 @@ class DBModule:
         with open("./auth/firebaseAuth.json") as f:
             config = json.load(f)
             
-        self.firebase = pyrebase.initialize_app(config)
+        firebase = pyrebase.initialize_app(config)
+        self.db = firebase.database()
 
-    def login(self, id, pwd):
-        pass
+    def login(self, uid, pwd):
+        users = self.db.child("users").get().val()
+        try:
+            userinfo = users[uid]
+            if userinfo["pwd"] == pwd:
+                return True
+            else:
+                return False
+        except:
+            return False
 
-    def register(self, id, pwd, name, email):
-        pass
+    def register_verification(self, uid):
+        users = self.db.child("users").get().val()
+        for i in users:
+            if uid == i:
+                return False
+
+        return True
+
+    def register(self, _id_, pwd, name, email):
+        information = {
+            "pwd" : pwd, 
+            "uname" : name, 
+            "email" : email
+        }
+        if self.register_verification(_id_):
+            self.db.child("users").child(_id_).set(information)
+            return True
+        else:
+            return False
--- server.py
+++ server.py
@@ -1,28 +1,59 @@
-from flask import Flask, redirect, render_template, url_for, request
+import re
+from flask import Flask, redirect, render_template, url_for, request, flash, session
 from DB_handler import DBModule
 
 app = Flask(__name__)
+app.secret_key = "asdfasdf@#aksdjflkasjlaksjdf!aksjf"
 DB = DBModule()
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if "uid" in session:
+        user = session["uid"]
+    else:
+        user = "Login"
+    return render_template("index.html", user = user)
 
 @app.route("/login")
 def login():
-    pass
+    if "uid" in session:
+        return redirect(url_for("index"))
+    return render_template("login.html")
 
-@app.route("/login_done")
+@app.route("/login_done", methods = ["get"])
 def login_done():
-    pass
+    uid = request.args.get("id")
+    pwd = request.args.get("pwd")
+    if DB.login(uid, pwd):
+        session["uid"] = uid
+        return redirect(url_for("index"))
+    else:
+        flash("아이디가 없거나 비밀번호가 틀립니다.")
+        return redirect(url_for("login"))
 
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-@app.route("/register_done")
+@app.route("/register_done", methods = ["get"])
 def register_done():
-    pass
+    email = request.args.get("email")
+    uid = request.args.get("id")
+    pwd = request.args.get("pwd")
+    name = request.args.get("name")
+    if DB.register(email = email, _id_ = uid, pwd = pwd, name = name):
+        return redirect(url_for("index"))
+    else:
+        flash("이미 존재하는 아이디 입니다.")
+        return redirect(url_for("register"))
+
+@app.route("/logout")
+def logout():
+    if "uid" in session:
+        session.pop("uid")
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("login"))   
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="12345", debug=True)
